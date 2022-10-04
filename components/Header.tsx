2fcce358db882eb58e0a56908cb6cf1df4a439c4
GiFTED! <gkenny896@gmail.com>
2022-10-04 06:38:29 +0100

    Fix header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,144 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => setOpen(!open);
+
   return (
-    <div className="w-full z-50 fixed top-0 border-0">
-      <div className="w-full bg-black border-0">
-        <div className="w-full text-white py-4 px-3 max-w-5xl mx-auto border-0 flex items-center justify-between">
-          <p className="">GiFTED!</p>
-          <p>About</p>
+    <>
+      <div className="w-full z-50 fixed top-0 border-0">
+        <div className="w-full bg-black border-0">
+          <div className="w-full text-white py-4 px-3 max-w-5xl mx-auto border-0 flex items-center justify-between">
+            <p className="">GiFTED!</p>
+            <p>About</p>
+          </div>
+        </div>
+        <div className="w-full bg-white">
+          <div className="text-black text-sm py-4 px-3 border-0 max-w-5xl mx-auto hidden md:flex items-center justify-between">
+            <a>GiFTED!</a>
+            <a href="#">Showcase</a>
+            <a href="#blog">Blog</a>
+            <a href="#companies">Companies</a>
+            <a href="#">About</a>
+            <a href="#contact">Contact</a>
+          </div>
+          <div className="text-black text-sm py-4 px-3 border-0 max-w-5xl mx-auto md:hidden flex items-center justify-between">
+            <a>GiFTED!</a>
+            <div className="flex items-center">
+              <a className="mr-2" href="#contact">
+                Contact
+              </a>
+              <button
+                aria-label={!open ? "close menu" : "open menu"}
+                className="flex navbar_menuButton"
+                type="button"
+                onClick={toggle}
+              >
+                <div
+                  className={`menu-toggle_wrap ${
+                    open && "menu-toggle_expanded"
+                  }`}
+                ></div>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="w-full bg-white">
-        <div className="text-black text-sm py-4 px-3 border-0 max-w-5xl mx-auto flex items-center justify-between">
-          <a>GiFTED!</a>
-          <a href="#">Showcase</a>
-          <a href="#blog">Blog</a>
-          <a href="#companies">Companies</a>
-          <a href="#">About</a>
-          <a href="#contact">Contact</a>
+      {open && (
+        <div className="sticky top-[120px] z-[2000] block md:hidden">
+          <div className="mobile-menu-wrapper">
+            <div className="flex w-screen max-w-[100vw] bg-white z-[2000] absolute top-0 left-0 right-0 bottom-0 overflow-y-scroll h-screen">
+              <div className="px-3 w-full mx-auto">
+                <ul>
+                  <li>
+                    <a
+                      className="text-greyish-300 py-2.5 px-2 flex"
+                      href="/learn"
+                    >
+                      Learn
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-greyish-300 py-2.5 px-2 flex"
+                      title="Showcase"
+                      href="/showcase"
+                    >
+                      Showcase
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-greyish-300 py-2.5 px-2 flex"
+                      title="Documentation"
+                      href="/docs/getting-started"
+                    >
+                      Docs
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-greyish-300 py-2.5 px-2 flex"
+                      href="/blog"
+                    >
+                      Blog
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-greyish-300 py-2.5 px-2 flex"
+                      href="/analytics"
+                    >
+                      Analytics
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-greyish-300 py-2.5 px-2 flex"
+                      href="/commerce"
+                    >
+                      Commerce
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-greyish-300 py-2.5 px-2 flex"
+                      href="/examples"
+                    >
+                      Examples
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-greyish-300 py-2.5 px-2 flex"
+                      href="https://vercel.com/contact/sales?utm_source=next-site&amp;utm_medium=navbar&amp;utm_campaign=next-website"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      Enterprise
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/learn">
+                      <a
+                        className="text-greyish-300 py-2.5 px-2 flex"
+                        href="https://github.com/vercel/next.js"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        GitHub
+                      </a>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
